c2dae9d78763baea4c824d42d4df0e3a96c33c0c
islamghany <islamghany3@gmail.com>
2022-05-08 15:15:08 +0200

    added husjy with pre-commit hooks

--- src/App.js
+++ src/App.js
@@ -4,10 +4,6 @@ import ff from './statics/1254705.jpg';
 
 const Warning = React.lazy(() => import('./Warning'));
 
-let x = 10;
-ley y= 20;
-
-let z = 22;
 const App = () => {
   const [counter, setCounter] = React.useState(0);
   return (
